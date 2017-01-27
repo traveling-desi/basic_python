e77a36a74e321915905d985e78228bba9d08d72d
Nikhil Sarpotdar <sarpotd@yahoo.com>
2017-01-26 21:20:27 -0800

    Make some changes to programs.

--- HackerRank/fibonacciModified.py
+++ HackerRank/fibonacciModified.py
@@ -1,3 +1,36 @@
+'''
+We define a modified Fibonacci sequence using the following definition:
+
+Given terms  and  where , term  is computed using the following relation:
+For example, if term  and , term , term , term , and so on.
+
+Given three integers, , , and , compute and print term  of a modified Fibonacci sequence.
+
+Note: The value of  may far exceed the range of a -bit integer. Many submission languages have libraries that can handle such large results but, for those that don't (e.g., C++), you will need to be more creative in your solution to compensate for the limitations of your chosen submission language.
+
+Input Format
+
+A single line of three space-separated integers describing the respective values of , , and .
+
+Constraints
+
+ may far exceed the range of a -bit integer.
+Output Format
+
+Print a single integer denoting the value of term  in the modified Fibonacci sequence where the first two terms are  and .
+
+Sample Input
+
+0 1 5
+Sample Output
+
+5
+Explanation
+
+The first two terms of the sequence are  and , which gives us a modified Fibonacci sequence of . Because , we print term , which is .
+'''
+
+
 t1, t2, n = map(int, raw_input().split())
 
 
